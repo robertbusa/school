d2a68140897cfd540ada3ca7e485c7850a735541
Unity tanfolyam <haabanimation@gmail.com>
2022-05-17 21:07:32 +0200

    Variables

---
+++ Assets/MyfirstScript.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+public class MyfirstScript : MonoBehaviour
+{
+    void Start()
+    {
+        int myfirstVariable;
+        myfirstVariable = 5;
+
+        Debug.Log(myfirstVariable);   }
+
+}
